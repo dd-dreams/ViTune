716ecc4d02dfbdc64429ba43c081d5e44817946d
dd-dreams <80887265+dd-dreams@users.noreply.github.com>
2025-05-14 16:25:56 +0300

    optimize requests

--- providers/newpipe/src/main/java/app/vitune/providers/newpipe/NewPipeInit.kt
+++ providers/newpipe/src/main/java/app/vitune/providers/newpipe/NewPipeInit.kt
@@ -3,20 +3,22 @@ package app.vitune.providers.newpipe
 import org.schabi.newpipe.extractor.NewPipe
 import org.schabi.newpipe.extractor.ServiceList
 import org.schabi.newpipe.extractor.exceptions.ExtractionException
-import org.schabi.newpipe.extractor.stream.StreamInfo
+import org.schabi.newpipe.extractor.stream.StreamExtractor
 
 object NewPipeInit {
     init {
         NewPipe.init(DownloaderImpl.init(null))
     }
 
-    fun getAudioStreams(mediaId: String): String {
-        val infoNewPipe = StreamInfo.getInfo(
-            ServiceList.YouTube,
-            "https://youtube.com/watch?v=$mediaId"
-        ) ?: throw ExtractionException("couldn't get info about audio with NewPipe")
+    private fun getExtractor(url: String): StreamExtractor {
+        return ServiceList.YouTube.getStreamExtractor(url)
+            ?: throw ExtractionException("couldn't get info about audio with NewPipe")
+    }
 
-        val audioStream = infoNewPipe.audioStreams.maxByOrNull { it.averageBitrate }
+    fun getAudioStreams(mediaId: String): String {
+        val streams = getExtractor("https://youtube.com/watch?v=$mediaId")
+
+        val audioStream =  streams.audioStreams.maxByOrNull { it.bitrate }
             ?: throw ExtractionException("couldn't get valid audio stream with NewPipe")
 
         return audioStream.content.toString()
